8de5129c8b520f7d9d72d4118b12fe7137776fab
Adam Schuppler <schupada@fel.cvut.cz>
2024-05-03 20:20:37 +0200

    Final analysis

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
 import requests
-import json
-import openpyxl
+import matplotlib.pyplot as plt
+from scipy import stats
 
 def get_radon_data():
+    print("INFO: fetching radon data")
     kod_cast = np.array([])
     kod_obec = np.array([])
     radon = np.array([])
@@ -20,9 +21,11 @@ def get_radon_data():
         else:
             print("Failed to retrieve data. Status code: ", response.status_code)
     np.save('radondata.npy', {'kod_cast': kod_cast,'kod_obec': kod_obec, 'radon': radon})
+    print("INFO: saved radon data")
     return (kod_cast, radon)
 
 def get_population(kod_cast):
+    print("INFO: appending population")
     df = pd.read_csv('sldb2021_obyv_byt_cob_zsj.csv')
     # Filter the DataFrame to include only rows where uzemi_typ is "část obce" and ukaz_txt is "Počet obyvatel s obvyklým pobytem"
     df = df[df['uzemi_typ'] == 'část obce']
@@ -45,17 +48,25 @@ def get_population(kod_cast):
     return population
 
 def get_okres(kod_obec):
+    print("INFO: appending okresy")
     df = pd.read_csv('VAZ0043_0101_CS.csv')
-    okres = np.array([])
+    okresy = np.array([])
     for kod in kod_obec:
-        okr = df.loc[df['chodnota1'] == int(kod), 'text2'].values
-        if len(okr) > 0:
-            okres = np.append(okres, okr)
+        okres = df.loc[df['chodnota1'] == int(kod), 'text2'].values
+        if len(okres) > 0:
+            # Special case for Praha and Rychnov nad Kněžnou
+            if okres == "Praha":
+                okresy = np.append(okresy, "Hlavní město Praha")
+            elif okres == "Rychnov nad Kněžnou":
+                okresy = np.append(okresy, "Rychnov n.Kněžnou")
+            else:
+                okresy = np.append(okresy, okres)
         else:
-            okres = np.append(okres, np.nan)
-    return okres
+            okresy = np.append(okresy, np.nan)
+    return okresy
 
 def get_kraj(okresy):
+    print("INFO: appending kraje")
     df = pd.read_csv('CiselnikOkresu_mod.csv', sep=';')
     kraje = np.array([])
     for okres in okresy:
@@ -67,7 +78,7 @@ def get_kraj(okresy):
     return kraje
 
 def fill_table(kod_cast, radon, population, okres, kraj):
-
+    print("INFO: creating helper table")
     table = pd.DataFrame(
         {
             "kod_cast": kod_cast,
@@ -77,54 +88,209 @@ def fill_table(kod_cast, radon, population, okres, kraj):
             "kraj": kraj
         }
     )
-    print(table)
     table.to_csv('table.csv', index=False)
+    print("INFO: helper table saved")
 
-def prepare_cancer_data():
+# Pandas z nejakeho duvodu potrebuje vlastni funkci na weighted average
+def w_avg(df, values, weights):
+    d = df[values]
+    w = df[weights]
+    return (d * w).sum() / w.sum()
+
+def prepare_cancer_data_per_okres():
+    print("INFO: creating okres table")
     radon_table = pd.read_csv('table.csv')
-    # Group by 'okres' and calculate the weighted average of radon index
-    weighted_avg_radon = radon_table.groupby('okres').apply(lambda x: np.average(x['radon'], weights=x['populace'])).reset_index()
-
+    weighted_avg_radon = radon_table.groupby('okres').apply(w_avg, 'radon', 'populace').reset_index()
     # Rename the columns
     weighted_avg_radon.columns = ['okres', 'radon_average']
     weighted_avg_radon["vyskyt"] = np.nan
 
+    weighted_avg_radon.to_csv('weighted_avg_radon.csv', index=False)
+
     # Load the Excel file
     df_excel = pd.read_excel('nor-ds-okresy-incidence-10lete.xlsx')
-    print(df_excel)
 
     for row in range(37275,46590):
         if df_excel.iloc[row, 0] == '2017–2021' and df_excel.iloc[row, 3] == 'ZN průdušnice, průdušky a plíce (C33, C34)':
             weighted_avg_radon.loc[weighted_avg_radon['okres'] == df_excel.iloc[row, 2], 'vyskyt'] = df_excel.iloc[row, 12]
 
-    # Print the result
-    print(weighted_avg_radon)
-    weighted_avg_radon.to_csv('results.csv', index=False)
+    weighted_avg_radon.to_csv('results_okresy.csv', index=False)
+
+def prepare_cancer_data_per_kraj():
+    print("INFO: creating kraj table")
+    radon_table = pd.read_csv('table.csv')
+    weighted_avg_radon = radon_table.groupby('kraj').apply(w_avg, 'radon', 'populace').reset_index()
+    # Rename the columns
+    weighted_avg_radon.columns = ['kraj', 'radon_average']
+    weighted_avg_radon["vyskyt"] = np.nan
+    weighted_avg_radon["particulate_matter"] = np.nan
+
+    weighted_avg_radon.to_csv('weighted_avg_radon_kraje.csv', index=False)
+
+    # Load the Excel file
+    df_excel = pd.read_excel('nor-ds-kraje-incidence-10lete.xlsx')
+    air_quality_excel = pd.read_excel('320198210308.xlsx')
+
+    for row in range(43687,45383):
+        if str(df_excel.iloc[row, 0]) == '2019' and df_excel.iloc[row, 2] == 'ZN průdušnice, průdušky a plíce (C33, C34)':
+            weighted_avg_radon.loc[weighted_avg_radon['kraj'] == df_excel.iloc[row, 1], 'vyskyt'] = df_excel.iloc[row, 11]
+
+    # manually add air quality data
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Hlavní město Praha', 'particulate_matter'] = air_quality_excel.iloc[10, 2]
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Jihomoravský kraj', 'particulate_matter'] = air_quality_excel.iloc[40,2]
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Jihočeský kraj', 'particulate_matter'] = air_quality_excel.iloc[16,2]
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Karlovarský kraj', 'particulate_matter'] = air_quality_excel.iloc[22,2]
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Kraj Vysočina', 'particulate_matter'] = air_quality_excel.iloc[37,2]
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Královéhradecký kraj', 'particulate_matter'] = air_quality_excel.iloc[31,2]
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Liberecký kraj', 'particulate_matter'] = air_quality_excel.iloc[28,2]
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Moravskoslezský kraj', 'particulate_matter'] = air_quality_excel.iloc[49,2]
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Olomoucký kraj', 'particulate_matter'] = air_quality_excel.iloc[43,2]
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Pardubický kraj', 'particulate_matter'] = air_quality_excel.iloc[34,2]
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Plzeňský kraj', 'particulate_matter'] = air_quality_excel.iloc[19,2]
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Středočeský kraj', 'particulate_matter'] = air_quality_excel.iloc[13,2]
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Zlínský kraj', 'particulate_matter'] = air_quality_excel.iloc[46,2]
+    weighted_avg_radon.loc[weighted_avg_radon['kraj'] == 'Ústecký kraj', 'particulate_matter'] = air_quality_excel.iloc[25,2]
+
+    weighted_avg_radon.to_csv('results_kraje.csv', index=False)
 
 def analyse_data():
-    data = pd.read_csv('results_mod.csv')
-    
+    print("INFO: analysing data")
+    print("OKRESY RESULTS")
+    data = pd.read_csv('results_okresy.csv')
     # Convert string values to numeric types
     data['radon_average'] = pd.to_numeric(data['radon_average'], errors='coerce')
     data['vyskyt'] = pd.to_numeric(data['vyskyt'], errors='coerce')
-    
-    # Check for NaN values after conversion
+
+     # Check for NaN values after conversion
     radon_average_nan = data['radon_average'].isna().any()
     vyskyt_nan = data['vyskyt'].isna().any()
-    
-    print("NaN values in 'radon_average':", radon_average_nan)
-    print("NaN values in 'vyskyt':", vyskyt_nan)
-    
-    # Calculate correlation matrix if there are no NaN values
+
+    print("radon avg: ", data['radon_average'].mean())
+    print("vyskyt avg: ", data['vyskyt'].mean())
+
+    print("radon sd: ", data['radon_average'].std())
+    print("vyskyt sd: ", data['vyskyt'].std())
+
     if not radon_average_nan and not vyskyt_nan:
-        matrix = np.corrcoef(data['radon_average'], data['vyskyt'])
+        # Extract the columns as NumPy arrays
+        radon_average = data['radon_average'].to_numpy()
+        vyskyt = data['vyskyt'].to_numpy()
+        
+        # Compute the correlation matrix
+        matrix = np.corrcoef([radon_average, vyskyt])
         print(matrix)
     else:
         print("NaN values detected. Cannot calculate correlation matrix.")
 
+    print("KRAJE RESULTS")
+
+    data = pd.read_csv('results_kraje.csv')
+    
+    # Convert string values to numeric types
+    data['radon_average'] = pd.to_numeric(data['radon_average'], errors='coerce')
+    data['vyskyt'] = pd.to_numeric(data['vyskyt'], errors='coerce')
+    data['particulate_matter'] = pd.to_numeric(data['particulate_matter'], errors='coerce')
+    
+    # Check for NaN values after conversion
+    radon_average_nan = data['radon_average'].isna().any()
+    vyskyt_nan = data['vyskyt'].isna().any()
+    particulate_matter_nan = data['particulate_matter'].isna().any()
+
+    print("radon avg: ", data['radon_average'].mean())
+    print("vyskyt avg: ", data['vyskyt'].mean())
+    print("particulate_matter avg: ", data['particulate_matter'].mean())
+
+    print("radon sd: ", data['radon_average'].std())
+    print("vyskyt sd: ", data['vyskyt'].std())
+    print("particulate_matter sd: ", data['particulate_matter'].std())
+    
+    # Calculate correlation matrix if there are no NaN values
+    if not radon_average_nan and not vyskyt_nan and not particulate_matter_nan:
+        # Extract the columns as NumPy arrays
+        radon_average = data['radon_average'].to_numpy()
+        vyskyt = data['vyskyt'].to_numpy()
+        particulate_matter = data['particulate_matter'].to_numpy()
+        
+        # Compute the correlation matrix
+        matrix = np.corrcoef([radon_average, vyskyt, particulate_matter])
+        print(matrix)
+    else:
+        print("NaN values detected. Cannot calculate correlation matrix.")
+
+def calculate_regression_okresy():
+    print("OKRESY REGRESSION")
+    data = pd.read_csv('results_okresy.csv')
+    # Convert string values to numeric types
+    data['radon_average'] = pd.to_numeric(data['radon_average'], errors='coerce')
+    data['vyskyt'] = pd.to_numeric(data['vyskyt'], errors='coerce')
+
+    x = data['radon_average']
+    y = data['vyskyt']
+
+    slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+    print("slope: ", slope)
+    print("intercept", intercept)
+    print("p: ", p)
+
+    def myfunc(x):
+        return slope * x + intercept
+
+    mymodel = list(map(myfunc, x))
+
+    plt.scatter(x, y)
+    plt.plot(x, mymodel)
+    plt.xlabel("radonový index")
+    plt.ylabel("incidence novotvarů plic na 100 000 obyvatel")
+    #plt.legend()
+    plt.savefig('novotvary_dle_radonu_okresy.pdf')
+    plt.savefig('novotvary_dle_radonu_okresy.png')
+    plt.show()
+
+def calculate_regression_kraje():
+    print("KRAJE REGRESSION")
+    data = pd.read_csv('results_kraje.csv')
+    # Convert string values to numeric types
+    data['radon_average'] = pd.to_numeric(data['radon_average'], errors='coerce')
+    data['vyskyt'] = pd.to_numeric(data['vyskyt'], errors='coerce')
+    data['particulate_matter'] = pd.to_numeric(data['particulate_matter'], errors='coerce')
+
+    x = data['radon_average']
+    y = data['vyskyt']
+
+    slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+    print("slope: ", slope)
+    print("intercept", intercept)
+    print("p: ", p)
+
+    def myfunc(x):
+        return slope * x + intercept
+
+    mymodel = list(map(myfunc, x))
+
+    plt.scatter(x, y, color="red")
+    plt.plot(x, mymodel, color="red")
+    plt.xlabel("radonový index")
+    plt.ylabel("incidence novotvarů plic na 100 000 obyvatel")
+    plt.savefig('novotvary_dle_radonu_kraje.pdf')
+    plt.savefig('novotvary_dle_radonu_kraje.png')
+    plt.show()
+
+    x = data['particulate_matter']
+    slope, intercept, r, p, std_err = stats.linregress(x, y)
+    mymodel = list(map(myfunc, x))
+
+    plt.scatter(x, y, color="green")
+    plt.plot(x, mymodel, color="green")
+    plt.xlabel("pevné částice [t/km2]")
+    plt.ylabel("incidence novotvarů plic na 100 000 obyvatel")
+    plt.savefig('novotvary_dle_pevnych_castic_kraje.pdf')
+    plt.savefig('novotvary_dle_pevnych_castic_kraje.png')
+    plt.show()
+
 def main():
-    #analyse_data()
-    #prepare_cancer_data()
+    get_radon_data()
     data = np.load('radondata.npy', allow_pickle=True)
     kod_cast = data.item().get('kod_cast')
     kod_obec = data.item().get('kod_obec')
@@ -133,6 +299,11 @@ def main():
     okres = get_okres(kod_obec)
     kraje = get_kraj(okres)
     fill_table(kod_cast, radon, population, okres, kraje)
+    prepare_cancer_data_per_okres()
+    prepare_cancer_data_per_kraj()
+    analyse_data()
+    calculate_regression_okresy()
+    calculate_regression_kraje()
 
 if __name__ == "__main__":
     main()
